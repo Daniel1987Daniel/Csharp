68d3c0f7b3a47eb315b7ffcb5f32140d95b03448
Daniel1987Daniel <danielslad@gmail.com>
2022-05-04 21:56:35 +0200

    myFirstApp

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Daniel";
+
+            Console.WriteLine("\n" + "Welcome here in your first console app " + firstName + "." + "\n" + "\n" + "Yeah congrat!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
